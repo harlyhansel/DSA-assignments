52f6f42917804f1f6e356d97fa89a78ddd8cd396
harlyhansel <harlyhansel@hotmail.com>
2025-03-12 15:40:12 +0300

    Add files via upload

---
+++ Assignments 3/greet_user.py
@@ -0,0 +1,23 @@
+# This is a simple Python program to greet the user
+
+# Ask for the user's name
+name = input("Enter your name: ")
+
+# Print a greeting message
+print(f"Hello, {name}! Welcome to Python programming.")
+
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+print("Before swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
